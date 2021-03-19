51197d06f63b0e24c9e7eef6c3faa4144956771e
Matt <matthewgorgees@gmail.com>
2021-03-18 21:21:59 -0400

    Delete generatorscript.js

--- assets/js/generatorscript.js
+++
@@ -1,6 +0,0 @@
-function linkvertisedynamic(userid, url) {
-    var base_url = "https://link-to.net/" + userid + "/" + Math.random() * 1000 + "/dynamic/";
-    var href = base_url + "?r=" + btoa(encodeURI(url));
-    document.getElementById("output").href = ""
-    document.getElementById("output").innerHTML = href
-}
